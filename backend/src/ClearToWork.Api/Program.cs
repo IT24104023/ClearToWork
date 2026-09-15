85c0e4e02e868be0e579321dcb682b672ce8d748
IT24104023 <mohammedzakee2006@gmail.com>
2026-09-15 23:50:18 +0530

    Fix Swagger RouteTemplate for reverse-proxy routing

--- backend/src/ClearToWork.Api/Program.cs
+++ backend/src/ClearToWork.Api/Program.cs
@@ -107,12 +107,16 @@ using (var scope = app.Services.CreateScope())
 }
 
 // 7. HTTP Request Pipeline
-app.UseSwagger();
+app.UseSwagger(c =>
+{
+    c.RouteTemplate = "swagger/{documentName}/swagger.json";
+});
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "ClearToWork AI API v1");
     c.RoutePrefix = "swagger";
 });
+
 
 
 app.UseCors("AllowFrontendClients");
