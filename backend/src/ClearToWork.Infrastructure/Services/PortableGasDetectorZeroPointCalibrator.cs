357a8354a79faea5e151751c5016d3a2095b8d3e
Oshini <IT24103874@my.sliit.lk>
2026-09-26 04:53:00 +0530

    feat(equipment): add PortableGasDetectorZeroPointCalibrator

---
+++ backend/src/ClearToWork.Infrastructure/Services/PortableGasDetectorZeroPointCalibrator.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace ClearToWork.Infrastructure.Services
+{
+    public class PortableGasDetectorZeroPointCalibrator
+    {
+        public bool CalibrateFreshAirZero(string serialNumber) => true;
+    }
+}
