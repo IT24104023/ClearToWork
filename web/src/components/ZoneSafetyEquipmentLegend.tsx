33b895699b03d798d2f56e83909b6b033e262c3e
Chemini <IT24104054@my.sliit.lk>
2026-09-26 05:50:00 +0530

    feat(ui): add ZoneSafetyEquipmentLegend component

---
+++ web/src/components/ZoneSafetyEquipmentLegend.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const ZoneSafetyEquipmentLegend: React.FC = () => (
+  <div className="legend-box">
+    <span>🎧 Double Hearing Protection Required</span>
+    <span>🔥 Continuous Gas Detection Active</span>
+  </div>
+);
